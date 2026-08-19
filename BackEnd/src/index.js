fb13b7665891f4664239d1c00704cabdfec0e8af
Cristian <cristimocan1005@gmail.com>
2026-08-19 19:33:11 +0300

    ambele

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import cookieParser from 'cookie-parser'
 import { connectDB } from './lib/db.js'
 const app = express()
@@ -9,6 +10,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
 
 const port = process.env.PORT || 5001
 app.listen(port, ()=>{

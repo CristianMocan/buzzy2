fca28287979f6b6ac1c20a28cc6908620057164e
Cristian Mocan <cristimocan1005@gmail.com>
2026-08-19 15:09:36 +0300

    Auth and cloudinary

--- BackEnd/src/controllers/auth.controller.js
+++ BackEnd/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"  
 import { generateToken } from "../lib/utils.js";
-import bycript from "bcryptjs"
+import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js";
 export const signup = async (req,res) =>{
     const {fullName, email, password} = req.body;
     try {
@@ -14,8 +15,8 @@ export const signup = async (req,res) =>{
         if (user){
             return res.status(400).send({message: "User already exists"})
         }
-        const salt = await bycript.genSalt(10)
-        const hashedPassword = await bycript.hash(password, salt);
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
         const newUser = new User({
             fullName,
             email,
@@ -26,9 +27,9 @@ export const signup = async (req,res) =>{
             await newUser.save();
             res.status(201).json({
                 _id: newUser._id,
-                fullName: newUser.fullName,
-                email: newUser.email,   
-                profilePic: newUser.profilePic,
+                fullName: newUser._fullName,
+                email: newUser._email,   
+                profilePic: newUser._profilePic,
             })
         }
     }catch(error){
@@ -37,6 +38,61 @@ export const signup = async (req,res) =>{
     }
 }
 
-export const login = (req,res) =>{
-    res.status(200).send("login route")
+export const login = async (req,res) =>{
+    const {email, password} = req.body
+    try{
+        const user = await User.findOne({email})
+        if (!user){
+            return res.status(400).json({message:"Userul nu exista"});
+        }
+        const isPass = await bcrypt.compare(password, user.password);
+        if (!isPass){
+            return res.status(400).json({message:"Email corect dar parola este gresita"});
+        }
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user._fullName,
+            email: user._email,
+            profilePic: user._profilePic
+        })
+    }catch(error){
+        console.log("Error in login controller", error.message);
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export const logout = (req,res) =>{
+    try{
+        res.cookie("jwt", "", {maxAge:0});
+        res.status(200).json({message: "Logged out succesfully"})
+    }catch(error){
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export const updateProfile = async (req,res) =>{
+    try{
+        const {profilePic} = req.body;
+        const userId = req.user._id
+        if(!profilePic){
+            return res.status(400).json({message: "Profile pic is required"})
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url},{new:true})
+        res.status(200).json(updatedUser)
+    }catch(error){
+        console.log("Error in update profile pic", error.message);
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export const checkAuth = (req,res) =>{
+    try{
+        res.status(200).json(req.user);
+    }catch(error){
+        console.log("Error in checkAuth", error.message);
+        res.status(500).json({message: "Internal server error"})
+    }
 }
--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 import { connectDB } from './lib/db.js'
 const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
 const port = process.env.PORT || 5001

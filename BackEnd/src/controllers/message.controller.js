48112a7b32ed4641f458e21eac98bd6233b0228d
Cristian Mocan <cristimocan1005@gmail.com>
2026-08-24 14:32:08 +0300

    update1

--- BackEnd/src/controllers/message.controller.js
+++ BackEnd/src/controllers/message.controller.js
@@ -1,16 +1,22 @@
+import User from "../models/user.model.js";
+import Message from '../models/message.model.js'
+
 export const getUserForSidebar = async (req, res) => {
-    try{
-        const loggedInUserId = req.user._id
+    try {
+        const loggedInUserId = req.user._id;
+
         const filteredUsers = await User.find({
-            _id: { $ne: loggedInUserId}.select("-password")
-        })
-        res.status(200).json(filteredUsers)
+            _id: { $ne: loggedInUserId }
+        }).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch (err) {
+        console.log("Error in getUserForSidebar controller", err);
+        res.status(500).send({
+            message: "Internal server error"
+        });
     }
-    catch(err){
-        console.log("Error in getUserForSidebar controller", err)
-        res.status(500).send({message:"Internal server error"})
-    }
-}
+};
 
 export const getMessages = async (req, res) => {
     try{

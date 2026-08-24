d642986ace0f7d3bbd48a5b725491a516dc5250f
Cristian <cristimocan1005@gmail.com>
2026-08-24 23:20:24 +0300

    aproape gata

--- FrontEnd/buzzy_app/src/store/useChatStore.js
+++ FrontEnd/buzzy_app/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import {create} from 'zustand';
 import toast from 'react-hot-toast'
 import { axiosInstance } from '../lib/axios';
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
     messages:[],
     users:[],
     selectedUser: null,
@@ -38,7 +38,9 @@ export const useChatStore = create((set) => ({
         const {selectedUser, messages} = get()
         try{
             const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData)
-            set({messages:[...messageData, res.data]})
+            set({
+            messages: [...messages, res.data]
+        });
         }catch(error){
             toast.error("Eroare la trimiterea mesajului")
         }

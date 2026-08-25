2f21bee01a31ab96483b0ec2bc851bdcd37b42c6
Cristian Mocan <cristimocan1005@gmail.com>
2026-08-25 17:37:52 +0300

    fix react router on vercel4

--- BackEnd/src/lib/socket.js
+++ BackEnd/src/lib/socket.js
@@ -6,7 +6,8 @@ const app = express();
 const server = http.createServer(app)
 const io = new Server(server, {
     cors:{
-        origin: ["https://buzzy1-fawn.vercel.app"]
+        origin: ["https://buzzy1-fawn.vercel.app"],
+        credentials: true
     }
 })
 

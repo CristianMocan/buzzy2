8e244ee30522682fc69e70d15d69050e17dbe7d8
Cristian Mocan <cristimocan1005@gmail.com>
2026-08-25 18:40:40 +0300

    4

--- BackEnd/src/lib/utils.js
+++ BackEnd/src/lib/utils.js
@@ -6,11 +6,11 @@ export const generateToken = (userId, res) =>{
         process.env.JWT_SECRET, 
         { expiresIn: "7D" }
     );
-    res.cookie("jwt", token, { 
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
-        samesite: "none", // Prevents the browser from sending this cookie along with cross-site requests
-        secure: process.env.NODE_ENV !== "development" // Set secure flag in production
-    });
+    res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true
+});
     return token;
 }
